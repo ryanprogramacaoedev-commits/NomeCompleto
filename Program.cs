22ae1a926dc7a5483d300b95d43c9ece0c0391d0
Ryan <ryanprogramacaoedev@gmail.com>
2025-09-24 20:09:39 -0300

    NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string nome, sobrenome;
+Console.Write("Digite o seu nome: ");
+nome = Console.ReadLine()!;
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+Console.Write($"Ola {nome} {sobrenome}");
